82a070e29c761db35053c70a538ef14268e40d1f
absaGH <absa3852@gmail.com>
2021-02-04 13:55:01 +0000

    c001

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - displays z-a in lowercase followed by newline
+ *Return: 0
+ */
+
+int main(void)
+{
+int n;
+for (n = 122; n > 96; n--)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
