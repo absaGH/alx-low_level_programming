a4e52840a38b85d84c98643f00b2ea983b0d2ffb
absaGH <absa3852@gmail.com>
2021-02-04 15:03:56 +0000

    c001

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,14 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
- *main - it prints out a-z in lowercase excluding letters 'e' and 'q'
- *Return: 0
+ * main - program that prints a-z in lowercase, excluding 'e' and 'q'
+ *followed by a new line
+ * Return: 0
  */
 
 int main(void)
 {
 int lwr = 'a';
-
 while (lwr <= 'z')
 {
 if (lwr == 'e' || lwr == 'q')
@@ -17,11 +17,10 @@ lwr += 1;
 }
 else
 {
-putchar('lwr');
+putchar(lwr);
 lwr += 1;
 }
 }
 putchar('\n');
 return (0);
 }
-
