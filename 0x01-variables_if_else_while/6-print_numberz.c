b4a3890d2a2a35ee628be0ea2c6ff210f5328297
absaGH <absa3852@gmail.com>
2021-02-04 13:47:37 +0000

    c001

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - program that displays all single digits in base 10
+ *without using char
+ *Return: 0
+ */
+
+int main(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
