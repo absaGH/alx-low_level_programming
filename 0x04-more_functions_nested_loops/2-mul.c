f499b7f478075ec97f99f78edf9917b71d6a2d4b
absaGH <absa3852@gmail.com>
2021-02-11 07:44:15 +0000

    0x04functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul -  a function that returns the multiplication of two integers
+ * @num1: input1
+ * @num2: input2
+ * Return: int value (i.e. product)
+ */
+int mul(int num1, int num2)
+{
+return (num1 * num2);
+}
