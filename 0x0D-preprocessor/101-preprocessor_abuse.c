f1978dc3ebe7546e00fcee1b920c6395d2efa9ac
absaGH <absa3852@gmail.com>
2021-03-13 14:38:44 +0000

    preprocessors

---
+++ 0x0D-preprocessor/101-preprocessor_abuse.c
@@ -0,0 +1,16 @@
+#ifndef _HEADER_
+#define _HEADER_
+#define MSG "Hello, Holberton"
+#endif
+#include <stdio.h>
+
+/**
+ * main - display Hello, Holberton message
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("%s\n", MSG);
+return (0);
+}
