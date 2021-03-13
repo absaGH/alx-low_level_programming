c4f2bc09fb0c7bb1f80be56bdc7d72625d940ec5
absaGH <absa3852@gmail.com>
2021-03-13 11:47:30 +0000

    structuresAndTypedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - displays the values of struct dog attributes
+ * @d: pointer to structure dog
+ *
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+if (d != NULL)
+{
+if (d->name != NULL)
+printf("Name: %s\n", d->name);
+else
+printf("Name: (nil)\n");
+printf("Age: %.6f\n", d->age);
+if (d->owner != NULL)
+printf("Owner: %s\n", d->owner);
+else
+printf("Owner: (nil)\n");
+}
+}
