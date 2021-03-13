2691cbd526feef811704a0444f19e20eaaba269c
absaGH <absa3852@gmail.com>
2021-03-13 14:22:52 +0000

    preprocessors

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - displays the name of the file from which
+ * the program is executed
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
