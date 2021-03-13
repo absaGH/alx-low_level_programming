4a1653cb6e82b997e24489496703b5dae67f852f
absaGH <absa3852@gmail.com>
2021-03-13 15:11:39 +0000

    argcAndargv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - displays the name of the program
+ * @argc: number of arguments passed to the function
+ * @argv: commanad line argument strings pointer
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
