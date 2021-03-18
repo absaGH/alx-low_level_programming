8a3c90de225d9e07011305444589371095a7c865
absaGH <absa3852@gmail.com>
2021-03-18 12:44:16 +0000

    singleLinkedList

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,13 @@
  */
 size_t print_list(const list_t *h)
 {
-list_t *tmp;
+const list_t *tmp;
 unsigned int i;
 
 tmp = h;
 for (i = 0; tmp; i++)
 {
-printf("[%u] %s\n", tmp->len, tmp->str);
+printf("[%lu] %s\n", tmp->len, tmp->str);
 tmp = tmp->next;
 }
 return (i);
