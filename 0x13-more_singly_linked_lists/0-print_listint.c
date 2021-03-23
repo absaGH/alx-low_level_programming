86a2c0131c11335bd42a1630737c5a1a4034936c
absaGH <absa3852@gmail.com>
2021-03-23 15:48:14 +0000

    moreSinglyLinkedList

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,7 +9,7 @@
  */
 size_t print_listint(const listint_t *h)
 {
-unsigned int count = 0;
+size_t count = 0;
 
 while (h != NULL)
 {
@@ -17,5 +17,6 @@ printf("%d\n", h->n);
 h = h->next;
 count++;
 }
+printf("-> %d elements\n", count);
 return (count);
 }
