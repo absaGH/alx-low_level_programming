ad5683f920d4c4ad5441eacf4eea5dd5278545c1
absaGH <absa3852@gmail.com>
2021-03-29 13:19:14 +0000

    fileprocessing

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,8 @@ return (i);
  * @filename: name of the file
  * @text_content: NULL terminated string to add at the end of the file
  *
- * Return: 1 on success and -1 on failure
+ * Return: 1 on success , -1 on failure
+ *         -1 if the file name is NULL
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
