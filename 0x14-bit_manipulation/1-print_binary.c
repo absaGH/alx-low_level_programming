6e703d0bbb1ee8fa73c378385352ef251f3940dd
absaGH <absa3852@gmail.com>
2021-04-19 11:16:20 +0000

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,35 +1,48 @@
+#include "holberton.h"
+
 /**
- * print_binary - Prints the binary representation of a number
- * @n: The number to representing in binary
+ * _pow - calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
  *
- * Return: Nothing
+ * Return: value of (base ^ power)
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+unsigned long int num;
+unsigned int i;
+
+num = 1;
+for (i = 1; i <= power; i++)
+num *= base;
+return (num);
+}
+
+/**
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ *
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-if (n == 0)
+unsigned long int divisor, check;
+char flag;
+
+flag = 0;
+divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+while (divisor != 0)
 {
-_putchar('0');
-return;
-}
-
-_divide(n);
-}
-
-/**
- * _divide - prints out the binary equivalent of a number equivalently
- * @n: the number to be converted to binary represenation
- *
- * Return: Nothing
- */
-void _divide(unsigned long int n)
+check = n & divisor;
+if (check == divisor)
 {
-if (n < 1)
-return;
-
-_divide(n >> 1);
-
-if (n & 1)
+flag = 1;
 _putchar('1');
-else
+}
+else if (flag == 1 || divisor == 1)
+{
 _putchar('0');
+}
+divisor >>= 1;
+}
 }
