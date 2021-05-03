50b17a94a15c2ca4aa1091f97124c01e6fd3902a
absaGH <absa3852@gmail.com>
2021-05-03 05:53:45 +0000

    dynamiclibrary

--- 0x18-dynamic_libraries/_atoi.c
+++ 0x18-dynamic_libraries/_atoi.c
@@ -13,17 +13,18 @@
  */
 int _atoi(char *s)
 {
-  int retval, i, neg;
+	int retval, i, neg;
 
-  i = 0;
-  retval = 0;
-  neg = -1;
-  while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
-    {
-      if (s[i] == '-')
-	neg *= -1;
-      i++;
-    } while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
-	retval = (retval * 10) - (s[i++] - '0');
-  return (retval *neg);
+	i = 0;
+	retval = 0;
+	neg = -1;
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+		retval = (retval * 10) - (s[i++] - '0');
+	return (retval * neg);
 }
