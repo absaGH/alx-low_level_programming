15ebc8668ecfa9a12d5daf59cc23795655011729
absaGH <absa3852@gmail.com>
2021-05-07 11:56:04 +0000

    DoublyLinkedList

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,27 +9,30 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp = *h, *new = NULL;
+	dlistint_t *temp = *h, *new;
 
 	if (idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
-	while (idx != 1)
+
+	for (; idx != 1; idx--)
 	{
-		idx--;
 		temp = temp->next;
 		if (temp == NULL)
 			return (NULL);
 	}
+
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
 	new->prev = temp;
 	new->next = temp->next;
 	temp->next->prev = new;
+	temp->next = new;
+
 	return (new);
 }
