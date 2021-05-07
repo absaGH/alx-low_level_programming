aa2997de44622dacafe7ba84c6a1f72e6f043f4f
absaGH <absa3852@gmail.com>
2021-05-07 11:51:30 +0000

    DoublyLinkedList

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,13 +8,10 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	size_t count = 0;
-
-	while (head != NULL)
+	for (; index != 0; index--)
 	{
-		if (count == index)
-			break;
-		count++;
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
 	}
 
