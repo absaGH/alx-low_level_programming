a4ec8cb75fd1d57339d6c14deecca9a1a74ee712
absaGH <absa3852@gmail.com>
2021-05-07 09:39:57 +0000

    DoublyLinkedList

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,44 +9,27 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count = 1;
-	dlistint_t *temp = NULL, *new = NULL;
+	dlistint_t *temp = *h, *new = NULL;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL || h == NULL)
-		return (NULL);
-	new->n = n;
-	temp = *h;
 	if (idx == 0)
 	{
-		*h = new;
-		new->next = temp;
-		new->prev = NULL;
-		temp->prev = new;
-		return (new);
+		return (add_dnodeint(h, n));
 	}
-	while (temp->next != NULL)
+	while (idx != 1)
 	{
-		if (count == idx)
-		{
-			new->prev = temp;
-			new->next = temp->next;
-			temp->next = new;
-			new->next->prev = new;
-		}
+		idx--;
 		temp = temp->next;
-		count++;
+		if (temp == NULL)
+			return (NULL);
 	}
-	if (count == idx)
-	{
-		new->prev = temp;
-		new->next = NULL;
-		temp->next = new;
-	}
-	if (count < idx)
-	{
-		free(new);
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
-	}
+	new->n = n;
+	new->prev = temp;
+	new->next = temp->next;
+	temp->next->prev = new;
 	return (new);
 }
