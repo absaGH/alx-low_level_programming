3fbecc75152e99cc8b78de51261f7b1ebf90f552
absaGH <absa3852@gmail.com>
2021-05-08 04:37:53 +0000

    DoublyLinkedList

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -29,7 +29,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	else
 	{
-		tmp->prev->next = tmp->next;
+		temp->prev->next = temp->next;
 		if (temp->next != NULL)
 			temp->next->prev = temp->prev;
 	}
