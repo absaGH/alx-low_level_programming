5655babfb6874b4f37b41d76c10ed78e848a0a5b
absaGH <absa3852@gmail.com>
2021-05-09 10:10:10 +0000

    singlyLinkedlist

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,20 +12,20 @@
  */
 const listint_t **printed(const listint_t **lst, size_t sz, const listint_t *n)
 {
-const listint_t **newlist;
-size_t i;
+	const listint_t **newlist;
+	size_t i;
 
-newlist = malloc(size * sizeof(listint_t *));
-if (newlist == NULL)
-{
-free(lst);
-exit(98);
-}
-for (i = 0; i < sz - 1; i++)
-newlist[i] = list[i];
-newlist[i] = n;
-free(lst);
-return (newlist);
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(lst);
+		exit(98);
+	}
+	for (i = 0; i < sz - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = n;
+	free(lst);
+	return (newlist);
 }
 
 /**
@@ -36,25 +36,25 @@ return (newlist);
  */
 size_t print_listint_safe(const listint_t *head)
 {
-size_t i, num = 0;
-const listint_t **list = NULL;
+	size_t i, num = 0;
+	const listint_t **list = NULL;
 
-while (head != NULL)
-{
-for (i = 0; i < num; i++)
-{
-if (head == list[i])
-{
-printf("-> [%p] %d\n", (void *)head, head->n);
-free(list);
-return (num);
-}
-}
-num++;
-list = printed(list, num, head);
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-}
-free(list);
-return (num);
+	while (head != NULL)
+	{
+		for (i = 0; i < num; i++)
+		{
+			if (head == list[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = printed(list, num, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+	}
+	free(list);
+	return (num);
 }
