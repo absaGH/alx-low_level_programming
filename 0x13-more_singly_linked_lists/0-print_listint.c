ef01e06ac23ff622160bb8746dbf317f5cf5a2be
absaGH <absa3852@gmail.com>
2021-05-09 09:11:59 +0000

    bettfix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+size_t count = 0;
 
-	while (h)
-	{
-		count++;
-		printf("%d\n", h->n);
-		h = h->next;
-	}
+while (h)
+{
+count++;
+printf("%d\n", h->n);
+h = h->next;
+}
 
-	return (count);
+return (count);
 }
